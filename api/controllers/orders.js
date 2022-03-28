590e2458265b513cb4767dcc3d56a97eb3ba0dfc
berke-cura <berkecura@protonmail.com>
2022-03-28 22:00:34 +0300

    Order status added to order model

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -3,11 +3,12 @@ const Order = require('../models/order');
 const Product = require('../models/product');
 
 const mongoose = require('Mongoose');
+const order = require('../models/order');
 
 
 exports.order_get_all = (req, res, next) => {
     Order.find()
-    .select('product quantity _id')
+    .select('product quantity _id status')
     .populate('product', 'name')
     .exec()
     .then(docs => {
@@ -18,6 +19,7 @@ exports.order_get_all = (req, res, next) => {
                     _id: doc._id,
                     product: doc.product,
                     quantity: doc.quantity,
+                    status: doc.status,
                     request: {
                         type: 'GET',
                         url: 'http://localhost:3000/orders/' + doc._id
@@ -117,4 +119,27 @@ exports.order_delete_order = (req, res, next) => {
         });
     });
     }); 
+};
+
+exports.order_set_orderStatus = (req, res, next) => {
+
+    if (!req.params.orderId.match(/^[0-9a-fA-F]{24}$/)) {
+        res.status(400).json({message: 'Order ID is invalid'})
+    } 
+
+    order.findByIdAndUpdate(req.params.orderId, {status: req.body.status}, {new: true, runValidators: true})
+        .then((doc) => {
+            if(!doc){
+                res.status(404).json({message: 'Order not found'})
+            }
+            res.status(200).json({
+                message: 'Order status updated',
+                order: doc
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({message: err.message})
+        })
+        
 };
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const checkAuth = require('../middleware/checkAuth');
 const OrdersControllers = require('../controllers/orders');
 
-router.get('/', checkAuth, OrdersControllers.order_get_all);
-router.post('/', checkAuth, OrdersControllers.order_create_order);
-router.get('/:orderId', checkAuth, OrdersControllers.order_get_order); 
-router.delete('/:orderId', checkAuth, OrdersControllers.order_delete_order);
+router.get('/', OrdersControllers.order_get_all);
+router.post('/', OrdersControllers.order_create_order);
+router.get('/:orderId',  OrdersControllers.order_get_order); 
+router.delete('/:orderId',  OrdersControllers.order_delete_order);
+router.patch('/:orderId' , OrdersControllers.order_set_orderStatus);
 
 module.exports = router; 
